d329fc4f9d711c6a8bcc444d5168efe69011a05a
Lokman Hossen <lhmisho@gmail.com>
2018-12-27 00:35:21 +0600

    routable page mixin

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
+import datetime
 from django.db import models
 from django import forms
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
@@ -26,15 +28,46 @@ class BlogCategory(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-class BlogPage(Page):
+class BlogPage(RoutablePageMixin, Page):
     description = models.CharField(max_length=255, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('description', classname='full')
     ]
 
+    def get_posts(self):
+        return PostPage.objects.descendant_of(self).live()
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(BlogPage, self).get_context(request, *args, **kwargs)
+        context['posts'] = self.posts
+        context['blog_page'] = self
+        return context
+
+    @route(r'^tag/(?P<tag>[-\w]+)/$')
+    def post_by_tag(self, request, tag, *args, **kwargs):
+        self.search_type = 'tag'
+        self.search_term = tag
+        self.posts = self.get_posts().filter(tags__slug=tag)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^category/(?P<category>[-\w]+)/$')
+    def post_by_category(self, request, category, *args, **kwargs):
+        self.search_type = 'category'
+        self.search_term = category
+        self.posts = self.get_posts().filter(categories__slug=category)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^$')
+    def post_list(self, request, *args, **kwargs):
+        self.posts = self.get_posts()
+        return Page.serve(self, request, *args, **kwargs)
+
+
+
 class PostPage(Page):
     body = RichTextField(blank=True)
+    date = models.DateTimeField(verbose_name="Post date", default=datetime.datetime.today)
     categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
     categories_two = ParentalManyToManyField('blog.BlogCat2', blank=True)
     tags = ClusterTaggableManager(through='blog.BlogPageTag', blank=True)
@@ -45,6 +78,20 @@ class PostPage(Page):
         FieldPanel('categories_two', widget=forms.CheckboxSelectMultiple),
         FieldPanel('tags'),
     ]
+
+    settings_panels = Page.settings_panels + [
+        FieldPanel('date')
+    ]
+
+    @property
+    def blog_page(self):
+        return self.get_parent().specific
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(PostPage, self).get_context(request, *args, **kwargs)
+        context['blog_page'] = self.blog_page
+        context['post'] = self
+        return context
 
 @register_snippet
 class BlogCat2(models.Model):
