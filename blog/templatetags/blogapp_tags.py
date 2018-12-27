ead884104c5afc641629c239301d1221f15b3023
Lokman Hossen <lhmisho@gmail.com>
2018-12-27 21:59:32 +0600

    template tag customized

--- blog/templatetags/blogapp_tags.py
+++ blog/templatetags/blogapp_tags.py
@@ -1,4 +1,5 @@
 from django.template import Library, loader
+from ..models import BlogCategory as Category, Tag
 #from django.core.urlresolvers import resolve
 
 register = Library()
@@ -16,3 +17,22 @@ def post_date_url(post, blog_page):
         )
     )
     return url
+
+@register.inclusion_tag('blog/components/tags_list.html',
+                        takes_context=True)
+def tags_list(context, limit=None):
+    blog_page = context['blog_page']
+    tags = Tag.objects.all()
+    if limit:
+        tags = tags[:limit]
+    return {
+        'blog_page': blog_page, 'request': context['request'], 'tags': tags}
+
+@register.inclusion_tag('blog/components/categories_list.html',
+                        takes_context=True)
+def categories_list(context):
+    blog_page = context['blog_page']
+    categories = Category.objects.all()
+    return {
+        'blog_page': blog_page, 'request': context['request'],
+        'categories': categories}
