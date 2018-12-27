0ee49cbac9f76a83983b6961483fbeecec61be14
Lokman Hossen <lhmisho@gmail.com>
2018-12-27 21:42:23 +0600

    tamplatetags

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 import datetime
+from datetime import date
 from django.db import models
 from django import forms
+from django.http import Http404, HttpResponse
+from django.utils.dateformat import DateFormat
+from django.utils.formats import date_format
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
@@ -42,7 +46,34 @@ class BlogPage(RoutablePageMixin, Page):
         context = super(BlogPage, self).get_context(request, *args, **kwargs)
         context['posts'] = self.posts
         context['blog_page'] = self
+        context['search_type'] = getattr(self, 'search_type', "")
+        context['search_term'] = getattr(self, 'search_term', "")
         return context
+
+    @route(r'^(\d{4})/$')
+    @route(r'^(\d{4})/(\d{2})/$')
+    @route(r'^(\d{4})/(\d{2})/(\d{2})/$')
+    def post_by_date(self, request, year, month, day, *args, **kwargs):
+        self.posts = self.get_posts().filter(date__year=year)
+        self.search_type = 'date'
+        self.search_term = yaer
+
+        if month:
+            self.posts = self.posts.filter(date__month = month)
+            df = DateFormat(date(int(year), int(month), 1))
+            self.search_term = df.format('F Y')
+        if day:
+            self.posts = self.posts.filter(date__day = day)
+            self.search_term = date_format(date(int(year), int(month), int(day)))
+
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^(\d{4})/(\d{2})/(\d{2})/(.+)/$')
+    def post_by_date_slug(self, request, year, month, day, slug, *args, **kwargs):
+        post_page = self.get_posts().filter(slug=slug).first()
+        if not post_page:
+            raise Http404
+        return Page.serve(post_page, request, *args, **kwargs)
 
     @route(r'^tag/(?P<tag>[-\w]+)/$')
     def post_by_tag(self, request, tag, *args, **kwargs):
--- wagtail_tuto/settings/base.py
+++ wagtail_tuto/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'wagtail.contrib.modeladmin',
     'blog',
+    'blog.templatetags',
     'el_pagination',
 ]
 
