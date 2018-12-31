77c5e5c6c03daa50d11913b605c90d471ba5083a
Lokman Hossen <lhmisho@gmail.com>
2018-12-31 16:00:28 +0600

    form builder

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,10 @@ from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
                                          PageChooserPanel, StreamFieldPanel)
 from blog.blocks import TwoColumnBlock
 
+# for formbuilder
+from modelcluster.fields import ParentalKey
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
 from wagtail.snippets.models import register_snippet
 from wagtailmarkdown.edit_handlers import MarkdownPanel
 from wagtailmarkdown.fields import MarkdownField
@@ -197,3 +201,28 @@ class LandingPage(Page):
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
     ]
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    # intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        # FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email Notification Config"),
+    ]
+
+    def get_form_fields(self):
+        return self.form_fields.all()
